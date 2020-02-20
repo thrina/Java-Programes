c5708eb58785a90f562be2ce2a768d2d5209a884
thrina <mca@thrinadh.com>
2020-02-20 17:19:31 +0530

    this thrid comment on for loop
    3rd loop comment

--- Programs/String/com/duplicateNumbers/DuplicateNumbers.java
+++ Programs/String/com/duplicateNumbers/DuplicateNumbers.java
@@ -5,7 +5,7 @@ public class DuplicateNumbers {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int array[]= {9,2,3,3,5,9};
-		
+		// This is for looop
 		for(int i=0;i<=array.length;i++)
 		{
 			for(int j=i+1;j<array.length;j++)
